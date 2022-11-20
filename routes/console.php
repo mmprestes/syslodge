ad62ce1edf00ad46db65e3dbbb276165df49a689
Marcelo Prestes <marcelo.prestes@gmail.com>
2022-11-20 10:57:13 -0300

    New inspiration messages were added to inspiration console method

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('me', function () {
+    $this->comment(\Auth::user()->name);
+})->purpose('Display who am I');
